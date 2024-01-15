5a94ac56a48a9f8452d46e988bc3c57739814d84
PATNALASUREKHA <119839913+PATNALASUREKHA@users.noreply.github.com>
2024-01-15 22:00:38 +0530

    import java.util.Scanner; public class Fun{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         int x = sc.nextInt();         System.out.printf("%.2f",(2.54*x));     } }

---
+++ Inches_to_Centimeters.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Fun{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.printf("%.2f",(2.54*x));
+    }
+}
