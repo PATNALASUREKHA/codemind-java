24e5dc0d2d4976e3f117e45605fc170ce8b6473a
PATNALASUREKHA <119839913+PATNALASUREKHA@users.noreply.github.com>
2024-01-16 16:17:22 +0530

    import java.util.Scanner; public class Kmph{     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);          int x = sc.nextInt();          System.out.printf("%.2f",(x*0.277777));     } }

---
+++ kmph_to_mps.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Kmph{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+         int x = sc.nextInt();
+         System.out.printf("%.2f",(x*0.277777));
+    }
+}
