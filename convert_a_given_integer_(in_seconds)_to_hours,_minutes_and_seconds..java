86e2ebd7202da44ed55f2638467b45c393b87f85
PATNALASUREKHA <119839913+PATNALASUREKHA@users.noreply.github.com>
2024-02-20 11:36:26 +0530

    import java.util.*; public class Given{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int h = a/3600;         int m = (a%3600)/60;         int s = a%60;         System.out.printf("H:M:S-%d:%d:%d",h,m,s);     } }

--- convert_a_given_integer_(in_seconds)_to_hours,_minutes_and_seconds..java
+++ convert_a_given_integer_(in_seconds)_to_hours,_minutes_and_seconds..java
@@ -3,7 +3,7 @@ public class Given{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int h  = a/3600;
+        int h = a/3600;
         int m = (a%3600)/60;
         int s = a%60;
         System.out.printf("H:M:S-%d:%d:%d",h,m,s);
