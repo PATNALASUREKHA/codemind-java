405526baf7f4cf2c882a5761901305ce34f37a43
PATNALASUREKHA <119839913+PATNALASUREKHA@users.noreply.github.com>
2024-03-14 09:40:53 +0530

    import java.util.*; public class Count{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         String str = sc.nextLine();         System.out.print(str.length());     } }

---
+++ Find_the_Digits_Count.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Count{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.print(str.length());
+    }
+}
