5cff17f9c67c96b3af1d64e4e0aa797fa26745a9
PATNALASUREKHA <119839913+PATNALASUREKHA@users.noreply.github.com>
2024-03-14 11:36:08 +0530

    import java.util.*; public class Range{     public static void main(String[] args){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         for(int i=a;i<=b;i++){             System.out.printf("%d ",i);         }     } }

---
+++ A_to_B.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Range{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int i=a;i<=b;i++){
+            System.out.printf("%d ",i);
+        }
+    }
+}
